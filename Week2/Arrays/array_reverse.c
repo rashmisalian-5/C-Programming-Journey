f0ef60986a9b6458825033bc7b90c7ed3a379b93
rashmisalian-5 <rashmiksalian5blue@gmail.com>
2026-01-28 19:45:47 +0530

    Add array reverse, count and linear search programs

---
+++ Week2/Arrays/array_reverse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    printf("Enter 5 numbers: ");
+    for(int i=0; i<5; i++) {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0; i<5; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\nReversed order is: ");
+    for(int i=4; i>=0; i--) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
